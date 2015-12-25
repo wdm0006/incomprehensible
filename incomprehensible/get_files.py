737dfdbf33f9d7e9133b60348f64b8205b446960
Will Mcginnis <will@predikto.com>
2015-12-25 13:30:24 -0600

    added example to doctoring

--- incomprehensible/get_files.py
+++ incomprehensible/get_files.py
@@ -14,6 +14,28 @@ __author__ = 'willmcginnis'
 
 
 def get_files(x_in, extension):
+    """
+
+    Before:
+
+    >>>#root directory for this repository
+
+    After:
+
+    >>>[
+    >>>    '~/incomprehensible/setup.py',
+    >>>    '~/incomprehensible/incomprehensible/__init__.py',
+    >>>    '~/incomprehensible/incomprehensible/dedupe_dictlist.py',
+    >>>    '~/incomprehensible/incomprehensible/dict_pivot.py',
+    >>>    '~/incomprehensible/incomprehensible/flatten_list.py',
+    >>>    '~/incomprehensible/incomprehensible/get_files.py',
+    >>>    '~/incomprehensible/incomprehensible/split_strip_coerce.py'
+    >>>]
+
+    :param x_in: a directory to start searching under
+    :param extension: the extension to search for
+    :return:
+    """
     return (os.path.join(root, file) for root, dir, files in os.walk(x_in) for file in files if file.endswith(extension))
 
 if __name__ == '__main__':
